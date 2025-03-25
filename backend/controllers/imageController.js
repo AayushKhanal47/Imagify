e738862110878aca8ad3bbf6de693adefbfcea70
AayushKhanal47 <aayushkhanal810@gmail.com>
2025-03-25 11:42:45 +0530

    imageController which generates images

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -1,3 +1,6 @@
+import userModel from "../models/userModel";
+import FormData from "form-data";
+
 export const generateImage = async (req, res) => {
   try {
     const { userId, prompt } = res.body;
@@ -5,6 +8,36 @@ export const generateImage = async (req, res) => {
     if (!user || !prompt) {
       return res.json({ success: false, message: "Missing Details" });
     }
+    if (user.creditBalance <= 0 || userModel.creditBalance < 0) {
+      return res.json({
+        success: false,
+        message: "Missing Details",
+        creditBalance: user.creditBalance,
+      });
+    }
+    const formData = new FormData();
+    formData.append("prompt", prompt);
+    const { data } = await axios.post(
+      "https://clipdrop-api.co/text-to-image/v1",
+      formData,
+      {
+        headers: {
+          "x-api-key": process.env.CLIPDROP_API,
+        },
+        respnseType: "arraybuffer",
+      }
+    );
+    const base64Image = Buffer.form(data, "binary").toString("base64");
+    const resultImage = "data:image/png;base64,${bases64Image}";
+    await userModel.findByIdAndUpdate(user._id, {
+      creditBalance: user.creditBalance - 1,
+    });
+    res.json({
+      success: true,
+      message: "Image Generated",
+      creditBalance: user.creditBalance - 1,
+      resultImage,
+    });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
